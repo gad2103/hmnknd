f6eda2bcdd4e2002b3f483c236eaf631e5581d3d
gabriel duncan <gad2103@gmail.com>
2014-02-13 18:03:18 -0500

    initial commit of hmnknd wordpress theme.

---
+++ functions.php
@@ -0,0 +1,45 @@
+<?php
+
+  // Initialize Redux Options Framework
+  if ( !class_exists( 'ReduxFramework' ) && file_exists( dirname( __FILE__ ) . '/admin/redux-options-framework/ReduxCore/framework.php' ) ) {
+    require_once( dirname( __FILE__ ) . '/admin/redux-options-framework/ReduxCore/framework.php' );
+  }
+  if ( !isset( $redux_demo ) && file_exists( dirname( __FILE__ ) . '/admin/redux-options-framework/sample/sample-config.php' ) ) {
+    require_once( dirname( __FILE__ ) . '/admin/redux-options-framework/sample/sample-config.php' );
+  }
+
+  // Custom Framework Includes
+  require_once locate_template('/lib/activation.php');
+
+	add_theme_support('post-formats');
+	add_theme_support('post-thumbnails');
+	add_theme_support('menus');
+
+	add_filter('get_twig', 'add_to_twig');
+	add_filter('timber_context', 'add_to_context');
+
+	add_action('wp_enqueue_scripts', 'load_scripts');
+
+	define('THEME_URL', get_template_directory_uri());
+	function add_to_context($data){
+		/* this is where you can add your own data to Timber's context object */
+		$data['qux'] = 'I am a value set in your functions.php file';
+		$data['menu'] = new TimberMenu();
+		return $data;
+	}
+
+	function add_to_twig($twig){
+		/* this is where you can add your own fuctions to twig */
+		$twig->addExtension(new Twig_Extension_StringLoader());
+		$twig->addFilter('myfoo', new Twig_Filter_Function('myfoo'));
+		return $twig;
+	}
+
+	function myfoo($text){
+    	$text .= ' bar!';
+    	return $text;
+	}
+
+	function load_scripts(){
+		wp_enqueue_script('jquery');
+	}
